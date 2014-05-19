1cf734761da8eb0070ceaf9a3bcce1954e12446f
kmiya <kmiya@outlook.com>
2014-05-20 02:17:48 +0900

    solved the problem in p.52 (p.60)

---
+++ ant-bk/p52_p60.cc
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  constexpr ll INF = numeric_limits<ll>::max();
+  int TEST;
+  cin >> TEST;
+  for (int test = 0; test < TEST; ++test) {
+
+    int n;
+    ll W;
+    cin >> W >> n;
+    vector<int> v;
+    vector<ll> w;
+    for (int i = 0; i < n; ++i) {
+      int tv, tw;
+      cin >> tw >> tv;
+      v.push_back(tv);
+      w.push_back(tw);
+    }
+
+    const int V_SUM = accumulate(v.begin(), v.end(), 0);
+    vector<vector<ll>> dp(V_SUM + 1, vector<ll>(n, INF));
+    for (int y = 0; y <= V_SUM; ++y)
+      if (y <= v[0]) dp[y][0] = w[0];
+
+    int acc = v[0];
+    for (int x = 1; x < n; ++x) {
+      for (int y = 0; y <= v[x] + acc; ++y) {
+        dp[y][x] = y <= v[x]
+                 ? min(dp[y][x - 1], w[x])
+                 : min(dp[y][x - 1], w[x] + dp[y - v[x]][x - 1]);
+      }
+      acc += v[x];
+    }
+
+    int ans = 0;
+    acc = 0;
+    for (int x = 0; x < n; ++x) {
+      for (int y = 0; y <= v[x] + acc; ++y) {
+        if (dp[y][x] <= W) ans = max(ans, y);
+      }
+      acc += v[x];
+    }
+    cout << "Case #" << test << ": " << ans << endl;
+
+    // // debug
+    // for (int x = 0; x < n; ++x) {
+    //   for (int y = 0; y <= V_SUM; ++y) {
+    //     if (dp[y][x] == INF) cout << "I ";
+    //     else if (dp[y][x] < 0) cout << "- ";
+    //     else cout << dp[y][x] << " ";
+    //   }
+    //   cout << endl;
+    // }
+  }
+}
