c24e428d4bdb7040c388cac5375a5b95e99304fc
Kazuki Miyahara <kmiya@outlook.com>
2022-04-29 00:39:50 +0900

    Reformat a macro

--- rustbook/ch10/wordcount/src/lib.rs
+++ rustbook/ch10/wordcount/src/lib.rs
@@ -100,9 +100,9 @@ mod test {
     }
 
     macro_rules! assert_map {
-    ($expr: expr, {$($key: expr => $value:expr),*}) => {
-        $(assert_eq!($expr[$key], $value));*
-    };
+        ($expr: expr, {$($key: expr => $value:expr),*}) => {
+            $(assert_eq!($expr[$key], $value));*
+        };
     }
 
     #[test]
