67515296feb636457341cd7b0a22d08331896d3c
Kazuki Miyahara <kmiya@outlook.com>
2023-03-11 13:06:50 +0900

    add constructor.c

---
+++ linker-loader/ch4/constructor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void __attribute__((constructor)) init() { printf("init()\n"); }
+
+void __attribute__((destructor)) fini() { printf("fini()\n"); }
+
+int main() {
+  printf("main()\n");
+  exit(0);
+}
