671b1319515138bb3f21408aeb64f3c601e6c52b
Kazuki Miyahara <kmiya@outlook.com>
2023-11-19 22:59:58 +0900

    add ch10 contents

---
+++ linker-loader/ch10/rasm.c
@@ -0,0 +1,20 @@
+extern int extvalue;
+extern int extbssvalue;
+int extfunc();
+
+static int dummy_value = 3;
+static int dummy_bssvalue;
+static int value = 1; /* .data section */
+static int bssvalue;  /* .bss section */
+char *string;
+
+static void dummy_func() {}
+
+static int func() { return (extvalue + extbssvalue + value + bssvalue); }
+
+int main() {
+  extfunc();
+  func();
+  string = "sample_string";
+  return (0);
+}
---
+++ linker-loader/ch10/rasm2.c
@@ -0,0 +1,8 @@
+int dummy_extvalue = 4;
+int dummy_extbssvalue;
+int extvalue = 2; /* .data section */
+int extbssvalue;  /* .bss section */
+
+void dummy_extfunc() {}
+
+int extfunc() { return (0); }
