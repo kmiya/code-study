3815120eea45b533774dcc81769a22fdb6967955
Kazuki Miyahara <kmiya@outlook.com>
2025-06-18 10:04:50 +0900

    Pass tests

--- command-line-rust/commr/src/main.rs
+++ command-line-rust/commr/src/main.rs
@@ -1,3 +1,150 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+use crate::Column::*;
+use anyhow::{Result, anyhow, bail};
+use clap::{ArgAction, Parser};
+use std::cmp::Ordering::{Equal, Greater, Less};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+struct Args {
+    /// Input file 1
+    #[arg()]
+    file1: String,
+
+    /// Input file 2
+    #[arg()]
+    file2: String,
+
+    /// Suppress printing of column 1
+    #[arg(short('1'), action(ArgAction::SetFalse))]
+    show_col1: bool,
+
+    /// Suppress printing of column 2
+    #[arg(short('2'), action(ArgAction::SetFalse))]
+    show_col2: bool,
+
+    /// Suppress printing of column 3
+    #[arg(short('3'), action(ArgAction::SetFalse))]
+    show_col3: bool,
+
+    /// Case-insensitive comparison of lines
+    #[arg(short)]
+    insensitive: bool,
+
+    /// Output delimiter
+    #[arg(short, long("output-delimiter"), default_value = "\t")]
+    delimiter: String,
+}
+
+enum Column<'a> {
+    Col1(&'a str),
+    Col2(&'a str),
+    Col3(&'a str),
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
+
+fn run(args: Args) -> Result<()> {
+    let file1_name = &args.file1;
+    let file2_name = &args.file2;
+
+    if file1_name == "-" && file2_name == "-" {
+        bail!(r#"Both input files cannot be STDIN ("-")"#);
+    }
+
+    let case = |line: String| {
+        if args.insensitive {
+            line.to_lowercase()
+        } else {
+            line
+        }
+    };
+
+    let mut lines1 = open(&args.file1)?.lines().map_while(Result::ok).map(case);
+    let mut lines2 = open(&args.file2)?.lines().map_while(Result::ok).map(case);
+
+    let print = |col: Column| {
+        let mut columns = vec![];
+        match col {
+            Col1(val) => {
+                if args.show_col1 {
+                    columns.push(val);
+                }
+            }
+            Col2(val) => {
+                if args.show_col2 {
+                    if args.show_col1 {
+                        columns.push("");
+                    }
+                    columns.push(val);
+                }
+            }
+            Col3(val) => {
+                if args.show_col3 {
+                    if args.show_col1 {
+                        columns.push("");
+                    }
+                    if args.show_col2 {
+                        columns.push("");
+                    }
+                    columns.push(val);
+                }
+            }
+        };
+
+        if !columns.is_empty() {
+            println!("{}", columns.join(&args.delimiter));
+        }
+    };
+
+    let mut line1 = lines1.next();
+    let mut line2 = lines2.next();
+
+    while line1.is_some() | line2.is_some() {
+        match (&line1, &line2) {
+            (Some(val1), Some(val2)) => match val1.cmp(val2) {
+                Equal => {
+                    print(Col3(val1));
+                    line1 = lines1.next();
+                    line2 = lines2.next();
+                }
+                Greater => {
+                    print(Col2(val2));
+                    line2 = lines2.next();
+                }
+                Less => {
+                    print(Col1(val1));
+                    line1 = lines1.next();
+                }
+            },
+            (Some(val1), None) => {
+                print(Col1(val1));
+                line1 = lines1.next();
+            }
+            (None, Some(val2)) => {
+                print(Col2(val2));
+                line2 = lines2.next();
+            }
+            (None, None) => unreachable!(),
+        }
+    }
+    Ok(())
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(
+            File::open(filename).map_err(|e| anyhow!("{filename}: {e}"))?,
+        ))),
+    }
 }
