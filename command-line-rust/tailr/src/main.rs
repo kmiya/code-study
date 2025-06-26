d0465947ce474d3df22d58e113d3fe19737c156d
Kazuki Miyahara <kmiya@outlook.com>
2025-06-26 23:38:11 +0900

    wip

--- command-line-rust/tailr/src/main.rs
+++ command-line-rust/tailr/src/main.rs
@@ -1,3 +1,41 @@
+use anyhow::Result;
+use clap::{arg, Parser};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `tail`
+struct Args {
+    /// Input file(s)
+    #[arg(required = true)]
+    files: Vec<String>,
+
+    /// Number of lines
+    #[arg(value_name = "LINES", short('n'), long, default_value = "10")]
+    lines: String,
+
+    /// Number of bytes
+    #[arg(value_name = "BYTES", short('c'), long, conflicts_with("lines"))]
+    bytes: Option<String>,
+
+    /// Suppress headers
+    #[arg(short, long)]
+    quiet: bool,
+}
+
+#[derive(Debug, PartialEq)]
+enum TakeValue {
+    PlusZero,
+    TakeNum(i64),
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
+
+fn run(args: Args) -> Result<()> {
+    println!("{:#?}", args);
+    Ok(())
 }
