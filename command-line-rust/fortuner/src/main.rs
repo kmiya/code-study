74576a8e31d1c72e183bbfe01e9a9dd3a0a84c57
Kazuki Miyahara <kmiya@outlook.com>
2025-07-08 20:30:43 +0900

    Pass `dies_bad_seed` test

--- command-line-rust/fortuner/src/main.rs
+++ command-line-rust/fortuner/src/main.rs
@@ -1,3 +1,35 @@
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `fortune`
+struct Args {
+    /// Input files or directories
+    #[arg(required(true), value_name = "FILE")]
+    sources: Vec<String>,
+
+    /// Pattern
+    #[arg(short('m'), long)]
+    pattern: Option<String>,
+
+    /// Case-insensitive pattern matching
+    #[arg(short, long)]
+    insensitive: bool,
+
+    /// Random seed
+    #[arg(short, long, value_parser(clap::value_parser!(u64)))]
+    seed: Option<u64>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
+
+fn run(args: Args) -> Result<()> {
+    println!("{:#?}", args);
+    Ok(())
 }
