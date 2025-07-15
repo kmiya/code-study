c392613b6d479e70ec1c5cc19d25ab8fbef2d04b
Kazuki Miyahara <kmiya@outlook.com>
2025-07-16 08:47:07 +0900

    Add tests

--- command-line-rust/fortuner/src/main.rs
+++ command-line-rust/fortuner/src/main.rs
@@ -1,5 +1,9 @@
+use std::path::PathBuf;
+
 use anyhow::Result;
+use anyhow::anyhow;
 use clap::Parser;
+use regex::RegexBuilder;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
@@ -31,5 +35,69 @@ fn main() {
 
 fn run(args: Args) -> Result<()> {
     println!("{:#?}", args);
+    let pattern = args
+        .pattern
+        .map(|val: String| {
+            RegexBuilder::new(val.as_str())
+                .case_insensitive(args.insensitive)
+                .build()
+                .map_err(|_| anyhow!(r#"Invalid --pattern "{val}""#))
+        })
+        .transpose()?;
     Ok(())
+}
+
+fn find_files(paths: &[String]) -> Result<Vec<PathBuf>> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::find_files;
+
+    #[test]
+    fn test_find_files() {
+        // Verify that the function finds a file known to exist
+        let res = find_files(&["./tests/inputs/jokes".to_string()]);
+        assert!(res.is_ok());
+
+        let files = res.unwrap();
+        assert_eq!(files.len(), 1);
+        assert_eq!(
+            files.get(0).unwrap().to_string_lossy(),
+            "./tests/inputs/jokes"
+        );
+
+        // Fails to find a bad file
+        let res = find_files(&["/path/does/not/exist".to_string()]);
+        assert!(res.is_err());
+
+        // Finds all the input files, excludes ".dat"
+        let res = find_files(&["./tests/inputs".to_string()]);
+        assert!(res.is_ok());
+
+        // Check number and order of files
+        let files = res.unwrap();
+        assert_eq!(files.len(), 5);
+        let first = files.get(0).unwrap().display().to_string();
+        assert!(first.contains("ascii-art"));
+        let last = files.last().unwrap().display().to_string();
+        assert!(last.contains("quotes"));
+
+        // Test for multiple sources, path must be unique and sorted
+        let res = find_files(&[
+            "./tests/inputs/jokes".to_string(),
+            "./tests/inputs/ascii-art".to_string(),
+            "./tests/inputs/jokes".to_string(),
+        ]);
+        assert!(res.is_ok());
+        let files = res.unwrap();
+        assert_eq!(files.len(), 2);
+        if let Some(filename) = files.first().unwrap().file_name() {
+            assert_eq!(filename.to_string_lossy(), "ascii-art".to_string())
+        }
+        if let Some(filename) = files.last().unwrap().file_name() {
+            assert_eq!(filename.to_string_lossy(), "jokes".to_string())
+        }
+    }
 }
