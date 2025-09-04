cf43527a699ad74a60b81cad24e1e91f83143b9f
Kazuki Miyahara <kmiya@outlook.com>
2025-09-04 23:20:51 +0900

    wip

--- command-line-rust/lsr/src/main.rs
+++ command-line-rust/lsr/src/main.rs
@@ -1,3 +1,129 @@
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+use anyhow::Result;
+use clap::{Parser, arg};
+use walkdir:walkdir;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `ls`
+struct Args {
+    /// Files and/or directories
+    #[arg(default_value = ".")]
+    paths: Vec<String>,
+
+    /// Long listing
+    #[arg(short, long)]
+    long: bool,
+
+    /// Show all files
+    #[arg(short('a'), long("all"))]
+    show_hidden: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
+
+fn run(args: Args) -> Result<()> {
+    println!("{:#?}", args);
+    Ok(())
+}
+
+fn find_files(paths: &[String], show_hidden: bool) -> Result<Vec<PathBuf>> {
+    let mut results = vec![];
+    for path in paths {
+        match fs::metadata(path) {
+            Err(e) => eprintln!("{path}: {e}"),
+            Ok(_) => results.extend(WalkDir::new(path).into_iter().filter_map(Result::ok)),
+        }
+    }
+    results.sort();
+    Ok(results)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::find_files;
+
+    #[test]
+    fn test_find_files() {
+        // Find all non-hidden entries in a directory
+        let res = find_files(&["tests/inputs".to_string()], false);
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            [
+                "tests/inputs/bustle.txt",
+                "tests/inputs/dir",
+                "tests/inputs/empty.txt",
+                "tests/inputs/fox.txt",
+            ]
+        );
+
+        // Any existing file should be found even if hidden
+        let res = find_files(&["tests/inputs/.hidden".to_string()], false);
+        assert!(res.is_ok());
+        let filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        assert_eq!(filenames, ["tests/inputs/.hidden"]);
+
+        // Test multiple path arguments
+        let res = find_files(
+            &[
+                "tests/inputs/bustle.txt".to_string(),
+                "tests/inputs/dir".to_string(),
+            ],
+            false,
+        );
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            ["tests/inputs/bustle.txt", "tests/inputs/dir/spiders.txt"]
+        );
+    }
+
+    #[test]
+    fn test_find_files_hidden() {
+        // Find all entries in a directory including hidden
+        let res = find_files(&["tests/inputs".to_string()], true);
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            [
+                "tests/inputs/.hidden",
+                "tests/inputs/bustle.txt",
+                "tests/inputs/dir",
+                "tests/inputs/empty.txt",
+                "tests/inputs/fox.txt",
+            ]
+        );
+    }
 }
